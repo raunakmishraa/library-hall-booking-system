3d2c0531ff58aa3d63bd3739d9e64a63235d1c32
Raunak Mishra <raunakraunak077@gmail.com>
2025-12-16 00:03:35 +0545

    redirects

--- backend/libraryHall/HallBook/urls.py
+++ backend/libraryHall/HallBook/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from . import views
-from django.contrib import admin
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('bookings', views.booking_calendar_view, name='booking_calendar_view'),
+    path('bookings/', views.booking_calendar_view, name='booking_calendar_view'),
     path('bookings/api/<int:booking_id>/', views.booking_detail_json, name='booking_detail_json'),
     path('bookings/update-status/<int:booking_id>/', views.update_booking_status, name='update_booking_status'),
 ]
